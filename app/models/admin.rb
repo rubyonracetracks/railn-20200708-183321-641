9cc83d94b21570fc10d0d2fe52d814e0e151e91f
Jason Hsu <rubyist@jasonhsu.com>
2020-07-08 18:52:42 +0000

    Added admin parameters

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -8,14 +8,31 @@
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  failed_attempts        :integer          default(0), not null
+#  unlock_token           :string
+#  locked_at              :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  name                   :string
+#  username               :string
+#  super                  :boolean
 #
 # Indexes
 #
+#  index_admins_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_admins_on_email                 (email) UNIQUE
 #  index_admins_on_reset_password_token  (reset_password_token) UNIQUE
-#
+#  index_admins_on_unlock_token          (unlock_token) UNIQUE
+#  index_admins_on_username              (username) UNIQUE
 #
 class Admin < ApplicationRecord
   # BEGIN: public section
@@ -24,5 +41,35 @@ class Admin < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
   # END: devise section
+
+  # BEGIN: constraints section
+  before_save :downcase_email, :downcase_username
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_USERNAME_REGEX = /\A[\w+\-.]+\z/i.freeze
+  validates :username, presence: true, length: { maximum: 255 },
+                       format: { with: VALID_USERNAME_REGEX },
+                       uniqueness: { case_sensitive: false }
+  # END: constraints section
   # END: public section
+
+  private
+
+  # BEGIN: private section
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Converts username to all lower-case.
+  def downcase_username
+    self.username = username.downcase
+  end
+  # END: private section
 end
